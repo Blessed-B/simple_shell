eaa151ceb565adf486cdd470fda6312282d0b92e
Lolo254 <odhiambo.laureen@gmail.com>
2023-05-25 02:36:44 -0700

    Code to please Betty

---
+++ proud_betty.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * add_num - adds two integers
+ * @a: iinteger to be added
+ * @b: integer to be added
+ * Return: sum of integers
+ */
+
+int add_num(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
